5386425caa009e3dcf43105f15ffb9083b90b928
yufuinn <sd16420@toyota-ti.ac.jp>
2016-06-07 15:33:10 +0900

    sample_rootを受け取って全部実行するように変更

--- comycome.py
+++ comycome.py
@@ -8,25 +8,6 @@ import re
 START = 5 # C5
 do_error_check = True
 
-if len(sys.argv) != 2:
-    # print("使い方  =>  $ comy.py root_dir_path template_file.path")
-    print("使い方  =>  $ comy.py root_dir_path")
-    # print("\"root_dir_path\"にはSumaC12_1c_XXXフォルダが入ってるフォルダのパスを渡してください")
-    print("\"root_dir_path\"にはSumaC12_1c_XXXフォルダのパスを渡してください")
-    exit(0)
-root_path = sys.argv[1]
-if not os.path.isdir(root_path):
-    print("\"" + root_path + "\"フォルダが見つかりません")
-    exit(-1)
-#template_path = sys.argv[2]
-# if not os.path.isfile(template_path):
-#     print("\"" + template_path + "\"ファイルが見つかりません")
-#     exit(-1)
-
-
-# 途中で作業ディレクトリを変えるので，最初に現在のディレクトリを記憶
-start_cwd = os.getcwd()
-
 # 一個一個のSumaC12_1c_XXX_YYYYY.chi（のうちのCの列）のデータとかを保存するやつ
 class Example:
     def __init__(self, filename): #filenameを受け取る
@@ -68,58 +49,92 @@ class Example:
         return
 
 
-# フォルダ中の全データを読み込み
-examples = []
+#------------------------------------実際の処理部分-----------------------------------------------------
+if len(sys.argv) != 2:
+    # print("使い方  =>  $ comy.py root_dir_path template_file.path")
+    print("使い方  =>  $ comy.py root_dir_path")
+    # print("\"root_dir_path\"にはSumaC12_1c_XXXフォルダが入ってるフォルダのパスを渡してください")
+    print("\"root_dir_path\"にはSumaC12_1c_XXXフォルダのパスを渡してください")
+    exit(0)
+root_path = os.path.abspath(sys.argv[1])
+if not os.path.isdir(root_path):
+    print("\"" + root_path + "\"フォルダが見つかりません")
+    exit(-1)
+#template_path = sys.argv[2]
+# if not os.path.isfile(template_path):
+#     print("\"" + template_path + "\"ファイルが見つかりません")
+#     exit(-1)
+
+
+# 途中で作業ディレクトリを変えるので，最初に現在のディレクトリを絶対パスで記憶
+start_cwd = os.path.abspath(os.getcwd())
+
+# コマンドライン引数で渡されたroot_pathに移動
 os.chdir(root_path)
-re_chi = re.compile("SumaC12_1c_[0-9]+_[0-9]+.chi")
-for filename in os.listdir("."):
-    if not re_chi.match(filename):
-        continue
-    examples.append(Example(filename))
-# 最後にdata_numでソート
-examples = sorted(examples, key=lambda e:e.data_num)
-# 最大行数
-max_row = max([len(e.c_column) for e in examples])
+# そのフォルダ中にある各ファイル・フォルダ名について
+for suma_path in os.listdir("."):
+    # まずはSumaC12_1c_TEMPの形式になっているかを確認
+    re_suma_folder = re.compile("^SumaC12_1c_([0-9]+)$")
+    if not re_suma_folder.match(suma_path): continue # 一致しないので次のファイル・フォルダへ
+    # 一致していた
+    temp_value = re_suma_folder.findall(suma_path)[0]
+    # imageフォルダの存在を確認
+    if not os.path.isdir(suma_path + "/image"): continue # 存在しないので次のファイル・フォルダへ
+    # 確認できた
+    # SumaC12_1c_TEMP/imageフォルダに移動
+    os.chdir(suma_path + "/image")
+    # フォルダ中の全データを読み込む
+    examples = []
+    re_chi = re.compile("SumaC12_1c_[0-9]+_[0-9]+.chi")
+    for filename in os.listdir("."):
+        if not re_chi.match(filename):
+            continue
+        examples.append(Example(filename))
+    # 最後にdata_numでソート
+    examples = sorted(examples, key=lambda e:e.data_num)
+    # 最大行数
+    max_row = max([len(e.c_column) for e in examples])
 
-# エラーチェック
-if do_error_check: #do_error_check = fals にしたら実行されない
-    isError = False
-    if len(examples) == 0:
-        print("実験データが見つからない")
-        exit(-1)
+    # エラーチェック
+    if do_error_check: #do_error_check = fals にしたら実行されない
+        isError = False
+        if len(examples) == 0:
+            print("実験データが見つからない")
+            exit(-1)
 
-    pre_num = 0
-    for i,e in enumerate(examples):
-        if i == 0:
-            if e.data_num != 1:
-                print("example_aaa_bbbのbbbが1から始まってない : 最初=>" + e.filename)
+        pre_num = 0
+        for i,e in enumerate(examples):
+            if i == 0:
+                if e.data_num != 1:
+                    print("example_aaa_bbbのbbbが1から始まってない : 最初=>" + e.filename)
+                    isError = True
+            else:
+                if examples[i-1].data_num + 1 != e.data_num:
+                    print("番号が続いてない : 前回=>" + examples[i-1].filename + "  今回=>" + e.filename)
+                    isError = True
+
+        first_ex_num = examples[0].ex_num
+        for e in examples[1:]:
+            if e.ex_num != first_ex_num:
+                print("実験番号が違う : " + e.filename)
                 isError = True
-        else:
-            if examples[i-1].data_num + 1 != e.data_num:
-                print("番号が続いてない : 前回=>" + examples[i-1].filename + "  今回=>" + e.filename)
-                isError = True
+        if isError:
+            exit(-1)
 
-    first_ex_num = examples[0].ex_num
-    for e in examples[1:]:
-        if e.ex_num != first_ex_num:
-            print("実験番号が違う : " + e.filename)
-            isError = True
-    if isError:
-        exit(-1)
-
-# まとめる
-os.chdir(start_cwd) #start_cwdに移動
-out_name = "alldata_" + examples[0].ex_num_str + ".csv"
-with open(out_name, mode='w') as out:
-    # はじめに1～5行目までを出力　空白
-    out.write("\n"*5)
-    for i in range(START-1, max_row):
-        line = "," # Aの列は空っぽ
-        for ex in examples:
-            if len(ex.c_column) > i:
-                line += ex.c_column[i]
-            line += ","
-        out.write(line + "\n")
-
-print("できました => " + out_name)
+    # まとめる
+    os.chdir("../") # 一つ上のフォルダ（imageフォルダがあるフォルダ）に移動
+    out_name = os.path.abspath("alldata_" + temp_value + ".csv")
+    # 書き込む
+    with open(out_name, mode='w') as out:
+        # はじめに1～5行目までを出力　空白
+        out.write("\n"*5)
+        for i in range(START-1, max_row):
+            line = "," # Aの列は空っぽ
+            for ex in examples:
+                if len(ex.c_column) > i:
+                    line += ex.c_column[i]
+                line += ","
+            out.write(line + "\n")
+    os.chdir(root_path) # 元のroot_pathに戻る
+    print("できました => " + os.path.relpath(out_name, start_cwd))
 
